d1b6d4e9f2ac4e0c352e9da1a0809c8311580bfe
Class Account <ee106a-aft@c105-9.local>
2021-11-17 13:40:12 -0800

    saving current project progress

--- project/src/forward_kinematics/src/move_to_position.py
+++ project/src/forward_kinematics/src/move_to_position.py
@@ -3,6 +3,7 @@
 
 import argparse
 import time
+import math
 
 import rospy
 
@@ -12,11 +13,37 @@ import intera_external_devices
 from intera_interface import CHECK_VERSION
 
 
-def convert_image_to_robot_coords(x, y):
+def convert_image_to_robot_coords(px, py, x_ofs=750, y_ofs=0, z_ofs=100):
     """
-    x = 0, y = 0 is the center of the paper
+    Input: px, py -- the number of millimeters from the center of the paper
+    Output: rx, ry, rz -- the coordinates of the joint above the end effector
+    in order to draw on the paper (in millimeters from the base of the robot)
+    rx is front/back direction from robot base (front is positive, back is negative)
+    ry is right/left direction
+    rz is up/down direction
     """
-    
+    # x_ofs is distance from robot base to center of paper in rx direction
+    # y_ofs is distance from robot base to center of paper in ry direction
+    # z_ofs is distance from desired joint position above end effector to robot base in rz direction
+    return py + x_ofs, -px + y_ofs, z_ofs
+
+def convert_robot_coords_to_joint_angles(rx, ry, rz):
+    """
+    Input: rx, ry, rz -- the coordinates of the joint above the end effector
+    in order to draw on the paper (in millimeters from the base of the robot)
+    Output: 7 joint angles
+    """
+    flat_dist = math.sqrt(
+
+    j1 = -math.atan2(192.50 - 168.50 + 136.30, 81 + 140 + 260 + 126.50 + 273.50)
+    j1 += math.atan2(ry, rx)
+    j2 = something
+    j3 = 0.0
+    j4 = something
+    j5 = 0.0
+    j6 = something
+    j7 = math.pi / 2
+
 
 def main():
     """RSDK Joint Position Example: Keyboard Control
