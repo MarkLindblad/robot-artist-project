e0e69f48c79c34653b8d0c41d2452ca6344da209
SamCribbs <90863504+SamCribbs@users.noreply.github.com>
2021-12-07 14:21:21 -0800

    Add files via upload

--- lines.py
+++ lines.py
@@ -66,9 +66,25 @@ def crosshatch(img, layers = 10, blacks = 0, whites = 1, brightness = 0, number 
         lines = np.vstack((lines, new))
 
     lines = lines[~np.all(lines == 0, axis = 1)]
+    print('Number of lines: ', lines.shape[0])
+
     return lines
 
-def cmyk_crosshatch(img, layers = 10, number = 30, angle = 35):
+def preview(lines, lw = 1):
+    fig, ax = plt.subplots(1, 2, figsize = (11, 5))
+
+    ax[0].imshow(crop(img), cmap = 'gray')
+    ax[0].axis('off')
+
+    for l in lines:
+        ax[1].plot(l[:2], l[2:], 'k-', lw = 1)
+    ax[1].axis([0, 1, 0, 1])
+    ax[1].axis('off')
+
+    plt.show()
+
+
+def cmyk_crosshatch(img, layers = 10, blacks = 0, whites = 1, number = 30, angle = 35):
     R = img[:, :, 2]
     G = img[:, :, 1]
     B = img[:, :, 0]
@@ -89,60 +105,57 @@ def cmyk_crosshatch(img, layers = 10, number = 30, angle = 35):
                 M[y, x] = 255*(255 - G[y, x] - K[y, x])/(255 - K[y, x])
                 Y[y, x] = 255*(255 - B[y, x] - K[y, x])/(255 - K[y, x])
     
-    clines = crosshatch(255 - C, layers = layers, number = number, angle = angle)
-    mlines = crosshatch(255 - M, layers = layers, number = number, angle = angle + 45)
-    ylines = crosshatch(255 - Y, layers = layers, number = number, angle = angle + 90)
-    klines = crosshatch(255 - K, layers = layers, number = number, angle = angle + 135)
+    clines = crosshatch(255 - C, layers = layers, blacks = blacks, whites = whites, number = number, angle = angle)
+    mlines = crosshatch(255 - M, layers = layers, blacks = blacks, whites = whites, number = number, angle = angle + 45)
+    ylines = crosshatch(255 - Y, layers = layers, blacks = blacks, whites = whites, number = number, angle = angle + 90)
+    klines = crosshatch(255 - K, layers = layers, blacks = blacks, whites = whites, number = number, angle = angle + 135)
+
+    print('Number of lines: ', clines.shape[0] + mlines.shape[0] + ylines.shape[0] + klines.shape[0])
 
     return clines, mlines, ylines, klines
 
-def levels(img):
-    fig, ax = plt.subplots(figsize = (5, 5))
-
-    ax.hist(crop(img)/255, 20, facecolor = 'k')
-    plt.show()
-
-if __name__ == "__main__":
-    # img = np.array(cv2.imread('mark.jpg'))
-
-    # fig, ax = plt.subplots(1, 2, figsize = (11, 5))
-
-    # ax[0].imshow(cv2.cvtColor(crop(img), cv2.COLOR_BGR2RGB))
-    # ax[0].axis('off')
-    
-    # clines, mlines, ylines, klines = cmyk_crosshatch(img, layers = 8, number = 30)
-
-    # lw = 2
-    # for l in clines:
-    #     ax[1].plot(l[:2], l[2:], color = 'cyan', lw = lw)
-    # for l in ylines:
-    #     ax[1].plot(l[:2], l[2:], color = 'yellow', lw = lw)
-    # for l in mlines:
-    #     ax[1].plot(l[:2], l[2:], color = 'magenta', lw = lw)
-    # for l in klines:
-    #     ax[1].plot(l[:2], l[2:], 'k-', lw = lw)
-    # ax[1].axis([0, 1, 0, 1])
-    # ax[1].axis('off')
-
-    # print('Number of lines: ', clines.shape[0] + mlines.shape[0] + ylines.shape[0] + klines.shape[0])
-    # plt.show()
-    
-    img = np.array(cv2.imread('ball2.jpg', 0))
-
+def cmyk_preview(clines, mlines, ylines, klines, lw = 2):
     fig, ax = plt.subplots(1, 2, figsize = (11, 5))
 
-    ax[0].imshow(crop(img), cmap = 'gray')
+    ax[0].imshow(cv2.cvtColor(crop(img), cv2.COLOR_BGR2RGB))
     ax[0].axis('off')
-    
-    lines = crosshatch(img, blacks = 0, whites = 0.9, layers = 7, number = 60)
-    for l in lines:
-        ax[1].plot(l[:2], l[2:], 'k-', lw = 1)
+
+    for l in clines:
+        ax[1].plot(l[:2], l[2:], color = 'cyan', lw = lw)
+    for l in ylines:
+        ax[1].plot(l[:2], l[2:], color = 'yellow', lw = lw)
+    for l in mlines:
+        ax[1].plot(l[:2], l[2:], color = 'magenta', lw = lw)
+    for l in klines:
+        ax[1].plot(l[:2], l[2:], 'k-', lw = lw)
     ax[1].axis([0, 1, 0, 1])
     ax[1].axis('off')
 
-    print('Number of lines: ', lines.shape[0])
-    f = open("ball2_lines.txt", "a")
-    for l in lines:
-        f.write(str(l) + '\n')
-    f.close()
     plt.show()
+
+def levels(img):
+    fig, ax = plt.subplots(figsize = (5, 5))
+    ax.hist(crop(img)/255, 20, facecolor = 'k')
+
+    plt.show()
+
+if __name__ == "__main__":
+    img = np.array(cv2.imread('ball2.jpg', 0))
+    lines = crosshatch(img, blacks = 0, whites = 0.9, layers = 7, number = 60)
+    preview(lines)
+
+    img = np.array(cv2.imread('steve.jpg', 0))
+    lines = crosshatch(img, blacks = 0, whites = 0.9, layers = 7, number = 60)
+    preview(lines)
+
+    img = np.array(cv2.imread('lady.jpg', 0))
+    lines = crosshatch(img, blacks = 0, whites = 0.85, layers = 7, number = 60)
+    preview(lines)
+
+    img = np.array(cv2.imread('man.png'))
+    clines, mlines, ylines, klines = cmyk_crosshatch(img, blacks = 0, whites = 0.9, layers = 5, number = 30)
+    cmyk_preview(clines, mlines, ylines, klines)
+
+    img = np.array(cv2.imread('starry.png'))
+    clines, mlines, ylines, klines = cmyk_crosshatch(img, blacks = 0, whites = 0.9, layers = 5, number = 30)
+    cmyk_preview(clines, mlines, ylines, klines)
